95481736b27fea2e8ceab0a5e0ccdca5a1226434
IV <137858242+Zyvoxi@users.noreply.github.com>
2025-03-09 22:40:45 -0300

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by IV. on 09/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
